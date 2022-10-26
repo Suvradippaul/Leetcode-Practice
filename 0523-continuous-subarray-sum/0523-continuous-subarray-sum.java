e9115c35db7384c6ea18189926f47111d4e06dbc
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-10-26 10:44:29 +0530

    Time: 64 ms (48.15%), Space: 116.9 MB (18.85%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,36 +1,19 @@
 class Solution {
     public boolean checkSubarraySum(int[] arr, int k) {
-        return solve(arr, k);
-    }
-    
-    boolean solve(int[] arr, int k) {
-		Map<Integer, Integer> map = new HashMap<>();
-		int sum = 0;
-		
-		for (int i = 0; i < arr.length; i++) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
 			sum += arr[i];
-			if (!map.containsKey(sum)) {
-				map.put(sum, i);
-			}
+            if (!map.containsKey(sum%k)) {
+                map.put(sum%k, i);
+            }
             else {
-				if (i - map.get(sum) >= 2) return true;
-			}
+                if (i - map.get(sum%k) >= 2) return true;
+            }
             if (sum % k == 0 && i > 0) return true;
-			if (sum - arr[0] < k) continue;
-			if (findMultiple(map, i, sum, k)) return true;
 		}
 		
 		return false;
-	}
-
-	boolean findMultiple(Map<Integer, Integer> map, int i, int currSum, int k) {
-		int multiplier = 1;
-		while (k * multiplier <= currSum) {
-			if (map.containsKey(currSum-k*multiplier)) {
-				if (i - map.get(currSum-k*multiplier) >= 2) return true;
-			}
-			multiplier++;
-		}
-		return false;
-	}
+    }
 }
