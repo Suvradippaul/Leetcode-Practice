87379187239590caf77636b04f21fd50ca064d1f
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-10-26 10:47:27 +0530

    Time: 77 ms (14.18%), Space: 109.4 MB (55.26%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -5,13 +5,13 @@ class Solution {
         
         for (int i = 0; i < arr.length; i++) {
 			sum += arr[i];
+            if (sum % k == 0 && i > 0) return true;
             if (!map.containsKey(sum%k)) {
                 map.put(sum%k, i);
             }
             else {
                 if (i - map.get(sum%k) >= 2) return true;
             }
-            if (sum % k == 0 && i > 0) return true;
 		}
 		
 		return false;
