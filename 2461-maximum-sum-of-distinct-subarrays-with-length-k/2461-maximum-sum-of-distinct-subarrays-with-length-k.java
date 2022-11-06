f086bba764058b72c832da4094b8f167f85c9ec5
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-11-06 20:40:10 +0530

    Time: 156 ms (20.00%), Space: 108.4 MB (20.00%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,30 +1,25 @@
 class Solution {
     public long maximumSubarraySum(int[] arr, int k) {
-        HashSet<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
 		int i = 0, j = 0;
 		long sum = 0;
 		long maxSum = 0;
-		
+
 		while (j < arr.length) {
-			if (!set.contains(arr[j])) {
-                set.add(arr[j]);
-                sum += arr[j];
-                if (j-i+1 == k) {
-					maxSum = Math.max(maxSum, sum);
-					sum -= arr[i];
-					set.remove(arr[i]);
-					i++;
+			map.put(arr[j], map.getOrDefault(arr[j], 0) + 1); // add it to map
+			sum += arr[j]; // add to local sum
+			if (j - i + 1 == k) { // if window length == K
+				if (map.size() == k) {      // if size of map is k, that means there are k unique elements
+					maxSum = Math.max(maxSum, sum); // take the max sum
 				}
+				sum -= arr[i]; // remove calculation of arr[i]
+				map.put(arr[i], map.get(arr[i])-1); // remove arr[i] from map. If arr[i] is duplicate in window, just decrease the freq by 1
+				if (map.get(arr[i]) == 0) map.remove(arr[i]);
+				i++; // shift the window to the right
 			}
-			else {
-				i = j;
-				sum = arr[i];
-				set = new HashSet<>();
-				set.add(arr[j]);
-			}
-			j++;
+			j++; // expand the window
 		}
-		
+
 		return maxSum;
     }
 }
