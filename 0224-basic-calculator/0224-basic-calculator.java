d31ee91dafe93ca992343e4db2c0134f1b752679
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-11-20 21:50:57 +0530

    Time: 25 ms (46.93%), Space: 45.9 MB (22.87%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,34 +1,55 @@
 class Solution {
     public int calculate(String s) {
-        s = s.trim();
-        int n = s.length();
-        int sign = 1;
-        int result = 0;
-        Stack<Integer> stack = new Stack<>();
-        
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            if (Character.isDigit(ch)) {
-                int sum = ch - '0';
-                while (i + 1 < n && Character.isDigit(s.charAt(i+1))) {
-                    sum = sum * 10 + (s.charAt(i+1) - '0');
-                    i++;
-                }
-                result += sum * sign;
-            }
-            else if (ch == '+') sign = 1;
-            else if (ch == '-') sign = -1;
-            else if (ch == '(') {
-                stack.push(result);
-                stack.push(sign);
-                result = 0;
-                sign = 1;
-            }
-            else if (ch == ')') {
-                result = result * stack.pop() + stack.pop();
-            }
-        }
-        
-        return result;
+        return solve(s, 0);
     }
+    
+    int j;
+    int solve(String s, int i) {
+		s = s.trim();
+		int n = s.length();
+		int num = 0;
+		Stack<Integer> stack = new Stack<>();
+		char sign = '+';
+		
+		while (i < n) {
+			char ch = s.charAt(i);
+			if (Character.isDigit(ch)) {
+				num = num * 10 + (ch - '0');
+			}
+			else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+				update(sign, num, stack);
+				num = 0;
+				sign = ch;
+			}
+            else if (ch == '(') {
+				num = solve(s, i+1);
+				i = j;
+			}
+			else if (ch == ')') {
+				update(sign, num, stack);
+				j = i;
+				return sum(stack);
+			}
+			i++;
+		}
+		
+		update(sign, num, stack);
+		
+		return sum(stack);
+	}
+	
+	int sum(Stack<Integer> stack) {
+		int sum = 0;
+		while (!stack.isEmpty()) {
+			sum += stack.pop();
+		}
+		return sum;
+	}
+
+	void update(char op, int val, Stack<Integer> stack) {
+		if (op == '+') stack.push(val);
+		else if (op == '-') stack.push(-val);
+		else if (op == '*') stack.push(stack.pop() * val);
+		else if (op == '/') stack.push(stack.pop() / val);
+	}
 }
