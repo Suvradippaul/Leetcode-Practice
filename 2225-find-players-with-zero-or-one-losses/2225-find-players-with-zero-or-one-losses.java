ebfb4efd2989f2427efe229042f17e4b34944dc5
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-11-28 09:46:07 +0530

    Time: 1379 ms (5.07%), Space: 151.9 MB (53.54%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,34 +1,33 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        Set<Integer> players = new HashSet<>();
-        Map<Integer, Integer> map = new HashMap<>();
+        int n = matches.length;
+        int[][] players = new int[100001][2];     // for all players --> participate, lose
         
         for (int[] match : matches) {
             int winner = match[0];
             int loser = match[1];
-            players.add(winner);
-            players.add(loser);
-            map.put(loser, map.getOrDefault(loser, 0) + 1);
+            
+            players[winner][0] = 1;
+            players[loser][0] = 1;    
+                
+            players[loser][1] += 1;
         }
         
         List<Integer> list0 = new ArrayList<>();
         List<Integer> list1 = new ArrayList<>();
-        for (int player : players) {
-            if (!map.containsKey(player)) {
-                list0.add(player);
-            }
-            else if (map.get(player) == 1) {
-                list1.add(player);
+        
+        for (int i = 1; i < players.length; i++) {
+            int[] player = players[i];
+            if (player[0] == 1) {
+                if (player[1] == 0) {
+                    list0.add(i);
+                }
+                else if (player[1] == 1) {
+                    list1.add(i);
+                }
             }
         }
         
-        Collections.sort(list0);
-        Collections.sort(list1);
-        
-        List<List<Integer>> ans = new ArrayList<>();
-        ans.add(list0);
-        ans.add(list1);
-        
-        return ans;
+        return new ArrayList<>(List.of(list0, list1));
     }
 }
