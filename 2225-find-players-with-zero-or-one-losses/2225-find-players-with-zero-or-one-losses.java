986fde89fd508fe92410497681318242e27ba79b
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-11-28 10:02:48 +0530

    Time: 196 ms (64.73%), Space: 111.8 MB (80.78%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
         int n = matches.length;
-        int[][] players = new int[100001][2];     // for all players --> participate, lose
+        int[] losers = new int[100001];
+        Set<Integer> players = new TreeSet<>();
         
         for (int[] match : matches) {
             int winner = match[0];
             int loser = match[1];
             
-            players[winner][0] = 1;
-            players[loser][0] = 1;    
+            players.add(winner);
+            players.add(loser);
                 
-            players[loser][1] += 1;
+            losers[loser] += 1;
         }
         
         List<Integer> list0 = new ArrayList<>();
         List<Integer> list1 = new ArrayList<>();
         
-        for (int i = 1; i < players.length; i++) {
-            int[] player = players[i];
-            if (player[0] == 1) {
-                if (player[1] == 0) {
-                    list0.add(i);
-                }
-                else if (player[1] == 1) {
-                    list1.add(i);
-                }
+        for (int player : players) {
+            if (losers[player] == 0) {
+                list0.add(player);
+            }
+            else if (losers[player] == 1) {
+                list1.add(player);
             }
         }
         
