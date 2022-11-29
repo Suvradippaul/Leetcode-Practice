3b72bfac8afede9e9b448249224bade329b80cab
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-11-29 20:55:07 +0530

    Time: 3 ms (78.38%), Space: 42.7 MB (65.40%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -12,53 +12,51 @@ class Triplet {
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
-        int m = grid[0].length;
-        
-        int[][] visited = new int[n][m];
-        ArrayDeque<Triplet> queue = new ArrayDeque<>();
-        
-        for (int i = 0; i < n; i++) {
-        	for (int j = 0; j < m; j++) {
-        		if (grid[i][j] == 2 && visited[i][j] != 2) {
-        			queue.add(new Triplet(i, j, 0));
-                    visited[i][j] = 2;
-        		}
-        	}
-        }
-        
-        int maxTime = 0;
-		while (!queue.isEmpty()) {
-			Triplet t = queue.poll();
-			int r = t.row;
-			int c = t.col;
-			int time = t.time;
-			
-			maxTime = Math.max(maxTime, time);
-			
-			if (r-1 >= 0 && grid[r-1][c] == 1 && visited[r-1][c] != 2) {
-				visited[r-1][c] = 2;
-				queue.add(new Triplet(r-1, c, time+1));
-			}
-			if (r+1 < grid.length && grid[r+1][c] == 1 && visited[r+1][c] != 2) {
-				visited[r+1][c] = 2;
-				queue.add(new Triplet(r+1, c, time+1));
-			}
-			if (c-1 >= 0 && grid[r][c-1] == 1 && visited[r][c-1] != 2) {
-				visited[r][c-1] = 2;
-				queue.add(new Triplet(r, c-1, time+1));
-			}
-			if (c+1 < grid[0].length && grid[r][c+1] == 1 && visited[r][c+1] != 2) {
-				visited[r][c+1] = 2;
-				queue.add(new Triplet(r, c+1, time+1));
+		int m = grid[0].length;
+
+		ArrayDeque<Triplet> queue = new ArrayDeque<>();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (grid[i][j] == 2) {
+					queue.add(new Triplet(i, j, 0));
+				}
 			}
 		}
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && visited[i][j] != 2) return -1;
-            }
-        }
-        
-        return maxTime;
+
+		int maxTime = 0;
+		while (!queue.isEmpty()) {
+			Triplet triplet = queue.poll();
+			int r = triplet.row;
+			int c = triplet.col;
+			int t = triplet.time;
+
+			maxTime = Math.max(maxTime, t);
+
+			if (r - 1 >= 0 && grid[r - 1][c] == 1) {
+				queue.add(new Triplet(r - 1, c, t + 1));
+				grid[r - 1][c] = 2;
+			}
+			if (r + 1 < n && grid[r + 1][c] == 1) {
+				queue.add(new Triplet(r + 1, c, t + 1));
+				grid[r + 1][c] = 2;
+			}
+			if (c - 1 >= 0 && grid[r][c - 1] == 1) {
+				queue.add(new Triplet(r, c - 1, t + 1));
+				grid[r][c - 1] = 2;
+			}
+			if (c + 1 < m && grid[r][c + 1] == 1) {
+				queue.add(new Triplet(r, c + 1, t + 1));
+				grid[r][c + 1] = 2;
+			}
+		}
+
+		for (int[] row : grid) {
+			for (int cell : row) {
+				if (cell == 1)
+					return -1;
+			}
+		}
+
+		return maxTime;
     }
 }
