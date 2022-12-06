6a2b91404c800abbacc247ca2358be0b8d555ed1
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-12-06 20:19:05 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.java
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.java
@@ -51,15 +51,13 @@ class Solution
         int n = grid.length;
 		int m = grid[0].length;
 
-		boolean[][] visited = new boolean[n][m];
 		int[][] ans = new int[n][m];
 
 		ArrayDeque<Triplet> queue = new ArrayDeque<>();
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				if (grid[i][j] == 1 && !visited[i][j]) {
+				if (grid[i][j] == 1) {
 					queue.add(new Triplet(i, j, 0));
-					visited[i][j] = true;
 					ans[i][j] = 0;
 				}
 			}
@@ -71,25 +69,25 @@ class Solution
 			int c = triplet.col;
 			int t = triplet.time;
 
-			if (r - 1 >= 0 && grid[r - 1][c] == 0 && !visited[r - 1][c]) {
+			if (r - 1 >= 0 && grid[r - 1][c] == 0) {
 				queue.add(new Triplet(r - 1, c, t + 1));
+				grid[r-1][c] = 1;
 				ans[r - 1][c] = t + 1;
-				visited[r - 1][c] = true;
 			}
-			if (r + 1 < n && grid[r + 1][c] == 0 && !visited[r + 1][c]) {
+			if (r + 1 < n && grid[r + 1][c] == 0) {
 				queue.add(new Triplet(r + 1, c, t + 1));
+				grid[r+1][c] = 1;
 				ans[r + 1][c] = t + 1;
-				visited[r + 1][c] = true;
 			}
-			if (c - 1 >= 0 && grid[r][c - 1] == 0 && !visited[r][c - 1]) {
+			if (c - 1 >= 0 && grid[r][c - 1] == 0) {
 				queue.add(new Triplet(r, c - 1, t + 1));
+				grid[r][c-1] = 1;
 				ans[r][c - 1] = t + 1;
-				visited[r][c - 1] = true;
 			}
-			if (c + 1 < m && grid[r][c + 1] == 0 && !visited[r][c + 1]) {
+			if (c + 1 < m && grid[r][c + 1] == 0) {
 				queue.add(new Triplet(r, c + 1, t + 1));
+				grid[r][c+1] = 1;
 				ans[r][c + 1] = t + 1;
-				visited[r][c + 1] = true;
 			}
 		}
 
