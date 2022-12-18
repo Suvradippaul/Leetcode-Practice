4722d44253f0783aea20b5c419987d620effcac4
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-12-18 19:34:45 +0530

    Time: 5 ms (70.00%), Space: 40.9 MB (80.00%) - LeetHub

--- 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -1,10 +1,9 @@
 class Solution {
     public int smallestValue(int n) {
-        int prev = n;
-        while (!isPrime(n)) {
-            n = sumOfPrimes(n);
-            if (prev == n) break;
-            prev = n;
+        int sum = sumOfPrimes(n);
+        while (sum != n) {
+            n = sum;
+            sum = sumOfPrimes(n);
         }
         
         return n;
@@ -23,11 +22,11 @@ class Solution {
         return sum;
     }
     
-    boolean isPrime(int n) {
-		if (n <= 1 || n%2 == 0 || n%3 == 0 || n%5 == 0 || n%7 == 0) return false;
-		for (int i = 2; i <= n/i; i++) {
-			if (n%i == 0) return false;
-		}
-		return true;
-	}
+//     boolean isPrime(int n) {
+// 		if (n <= 1 || n%2 == 0 || n%3 == 0 || n%5 == 0 || n%7 == 0) return false;
+// 		for (int i = 2; i <= n/i; i++) {
+// 			if (n%i == 0) return false;
+// 		}
+// 		return true;
+// 	}
 }
