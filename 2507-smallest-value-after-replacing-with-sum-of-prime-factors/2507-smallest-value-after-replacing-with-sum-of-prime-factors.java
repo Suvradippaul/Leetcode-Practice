bfff0b248f52bd01545560692391a7a0bdfbed05
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-12-18 10:28:03 +0530

    Time: 61 ms (40.00%), Space: 53.4 MB (10.00%) - LeetHub

---
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -0,0 +1,50 @@
+class Solution {
+    boolean[] isPrime;
+	boolean[] vis;
+	void fill(int n) {
+		isPrime = new boolean[n+1];
+		Arrays.fill(isPrime, true);
+		vis = new boolean[n+1];
+		
+		for (int i = 2; i < isPrime.length; i++) {
+			if (!vis[i]) {
+				int num = i;
+				int mul = 2;
+				while (num * mul < isPrime.length) {
+					int prod = num*mul;
+					isPrime[prod] = false;
+					vis[prod] = true;
+					mul++;
+				}
+			}
+		}
+	}
+    
+    public int smallestValue(int n) {
+        fill(n);
+        
+        int prev = n;
+        while (!isPrime[n]) {
+            n = sum(n);
+            if (prev == n) break;
+            prev = n;
+        }
+        
+        return n;
+    }
+    
+    int sum(int n) {
+        int add = 0;
+        int i = 2;
+        while (n != 1 && i <= n) {
+            if (isPrime[i] && n%i == 0) {
+                n /= i;
+                add += i;
+            }
+            else {
+                i++;
+            }
+        }
+        return add;
+    }
+}
