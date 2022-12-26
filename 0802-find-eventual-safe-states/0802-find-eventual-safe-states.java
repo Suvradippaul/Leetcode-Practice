950c46885c9e02def759e8625efc59c6d622fd05
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2022-12-26 21:48:24 +0530

    Time: 14 ms (53.91%), Space: 50.6 MB (89.03%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -0,0 +1,37 @@
+class Solution {
+    List<Integer> ans;
+    public List<Integer> eventualSafeNodes(int[][] adj) {
+        int V = adj.length;
+        ans = new ArrayList<>();
+        
+		int[] visited = new int[V];
+		int[] pathVisited = new int[V];
+		
+		for (int i = 0; i < V; i++) {
+			if (visited[i] == 0) {
+				isSafe(i, adj, visited, pathVisited);
+			}
+		}
+		
+		Collections.sort(ans);
+		return ans;
+    }
+    
+	private boolean isSafe(int i, int[][] adj, int[] visited, int[] pathVisited) {
+		visited[i] = 1;
+		pathVisited[i] = 1;
+		
+		for (int neighbour : adj[i]) {
+			if (visited[neighbour] == 0) {
+				if (!isSafe(neighbour, adj, visited, pathVisited)) return false;     // this path could not reach destination, so it cannot be a safe node.
+			}
+			else if (visited[neighbour] == 1 && pathVisited[neighbour] == 1) {  // a cycle is present. So cannot be a safe node
+				return false;
+			}
+		}
+		
+		pathVisited[i] = 0;
+		ans.add(i);
+		return true;
+	}
+}
