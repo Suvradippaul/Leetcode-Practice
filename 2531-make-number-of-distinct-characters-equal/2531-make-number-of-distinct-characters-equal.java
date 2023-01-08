a6e18a56c12284ce0822b2263df8f01fd9e9dad0
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2023-01-08 18:41:10 +0530

    Time: 11 ms (75.00%), Space: 43.5 MB (75.00%) - LeetHub

--- 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.java
+++ 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.java
@@ -3,39 +3,33 @@ class Solution {
         int[] map1 = new int[26];
         int[] map2 = new int[26];
         
-        for (char ch : word1.toCharArray()) map1[ch-'a']++;
-        for (char ch : word2.toCharArray()) map2[ch-'a']++;
+        for (int i = 0; i < word1.length(); i++) map1[word1.charAt(i)-'a']++;
+        for (int i = 0; i < word2.length(); i++) map2[word2.charAt(i)-'a']++;
         
-        for (int i = 0; i < 26; i++) {
-        	if (map1[i] == 0) continue;
-        	for (int j = 0; j < 26; j++) {
-        		if (map2[j] == 0) continue;
-        		
-        		map1[j]++;
-        		map1[i]--;
-        		map2[i]++;
-        		map2[j]--;
-        		
-        		if (same(map1, map2)) return true;
-        		
-        		map1[j]--;
-        		map1[i]++;
-        		map2[i]--;
-        		map2[j]++;
-        	}
-        }
-        
-        return false;
-    }
-    
-    boolean same(int[] map1, int[] map2) {
-        int count1 = 0;
-        int count2 = 0;
+        int count1 = 0, count2 = 0;
         for (int i = 0; i < 26; i++) {
             if (map1[i] > 0) count1++;
             if (map2[i] > 0) count2++;
         }
         
-        return count1 == count2;
+        if (count1 == count2 && word1.length() == word2.length()) return true;
+        
+        for (int i = 0; i < 26; i++) {
+        	for (int j = 0; j < 26; j++) {
+                if (map1[i] == 0 || map2[j] == 0 || i == j) continue;
+                
+                int tempCount1 = count1, tempCount2 = count2;
+                
+                if (map1[j] == 0) tempCount1++;
+                if (map1[i] == 1) tempCount1--;
+                
+                if (map2[i] == 0) tempCount2++;
+                if (map2[j] == 1) tempCount2--;
+                
+                if (tempCount1 == tempCount2) return true;
+            }
+        }
+        
+        return false;
     }
 }
