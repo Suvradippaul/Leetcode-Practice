863ccc89c3666106baa176e35b3a24d84fd59288
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2023-01-10 20:34:35 +0530

    Time: 10 ms (74.45%), Space: 43.4 MB (61.67%) - LeetHub

--- 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.java
+++ 2531-make-number-of-distinct-characters-equal/2531-make-number-of-distinct-characters-equal.java
@@ -3,33 +3,38 @@ class Solution {
         int[] map1 = new int[26];
         int[] map2 = new int[26];
         
-        for (int i = 0; i < word1.length(); i++) map1[word1.charAt(i)-'a']++;
-        for (int i = 0; i < word2.length(); i++) map2[word2.charAt(i)-'a']++;
-        
-        int count1 = 0, count2 = 0;
-        for (int i = 0; i < 26; i++) {
-            if (map1[i] > 0) count1++;
-            if (map2[i] > 0) count2++;
-        }
-        
-        if (count1 == count2 && word1.length() == word2.length()) return true;
+        for (char ch : word1.toCharArray()) map1[ch-'a']++;
+        for (char ch : word2.toCharArray()) map2[ch-'a']++;
         
         for (int i = 0; i < 26; i++) {
+        	if (map1[i] == 0) continue;
+        	char ch1 = (char)(i+'a');
         	for (int j = 0; j < 26; j++) {
-                if (map1[i] == 0 || map2[j] == 0 || i == j) continue;
-                
-                int tempCount1 = count1, tempCount2 = count2;
-                
-                if (map1[j] == 0) tempCount1++;
-                if (map1[i] == 1) tempCount1--;
-                
-                if (map2[i] == 0) tempCount2++;
-                if (map2[j] == 1) tempCount2--;
-                
-                if (tempCount1 == tempCount2) return true;
-            }
+        		if (map2[j] == 0) continue;
+        		char ch2 = (char)(j+'a');
+        		
+        		map1[ch2-'a']++;
+        		map1[ch1-'a']--;
+        		map2[ch1-'a']++;
+        		map2[ch2-'a']--;
+        		
+        		if (count(map1) == count(map2)) return true;
+        		
+        		map1[ch2-'a']--;
+        		map1[ch1-'a']++;
+        		map2[ch1-'a']--;
+        		map2[ch2-'a']++;
+        	}
         }
         
         return false;
     }
+    
+    int count(int[] map) {
+		int count = 0;
+		for (int n : map) {
+			if (n > 0) count++;
+		}
+		return count;
+	}
 }
