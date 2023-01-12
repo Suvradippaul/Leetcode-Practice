b01396fa9de70247ed23a3fab5fb4f2af30476c4
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2023-01-12 22:11:37 +0530

    Time: 78 ms (92.39%), Space: 112.9 MB (96.74%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
@@ -25,13 +25,8 @@ class Solution {
         for (int neighbour : adj.get(node)) {
             
             if (visited[neighbour] == 0) {
-                // HashMap<Character, Integer> newMap = new HashMap<>();
                 int[] newMap = new int[26];
                 solve(neighbour, adj, labels, visited, newMap, ans);
-				// for (var entry : newMap.entrySet()) {
-				// 	char ch = entry.getKey();
-				// 	map.put(ch, map.getOrDefault(ch, 0) + entry.getValue());
-				// }
                 for (int i = 0; i < 26; i++) {
                     map[i] += newMap[i];
                 }
@@ -39,10 +34,6 @@ class Solution {
         }
         
         int i = (int) (labels.charAt(node) - 'a');
-        // map.put(ch, map.getOrDefault(ch, 0)+1);
-        // ans[node] = map.get(ch);
-        
-        // map[i]++;
         ans[node] = ++map[i];
     }
 }
