7111a369873178df4cabec3a34aa9356ae856b5c
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2023-01-21 11:19:24 +0530

    Time: 6 ms (54.94%), Space: 43 MB (40.10%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -6,22 +6,20 @@ class Solution {
     }
     
     void solve(String s, int i, int parts, String str, List<String> ans) {
-        if (parts > 4) return;
+        if (parts > 4 || i > s.length()) return;
         if (i == s.length()) {
         	if (parts == 4) ans.add(str.substring(0, str.length()-1));
         	return;
         }
         
-        for (int index = i; index < s.length(); index++) {
+        for (int index = i; index < i+3 && index < s.length(); index++) {
             if (valid(s, i, index)) {
                 solve(s, index+1, parts+1, str + s.substring(i, index+1) + ".", ans);
             }
         }
     }
 
-	boolean valid(String s, int start, int index) {
-		if (index-start > 2) return false;
-		
+	boolean valid(String s, int start, int index) {	
 		int value = Integer.parseInt(s.substring(start, index+1));
 		if (value > 255) return false;
 		if (index-start == 2 && value < 100) return false;
