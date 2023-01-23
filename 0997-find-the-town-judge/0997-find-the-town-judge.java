f2a7010448ddc4a52fac93c8c8939e5eff1864b3
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2023-01-23 20:48:52 +0530

    Time: 2 ms (99.00%), Space: 50 MB (74.78%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,17 +1,15 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if (trust.length == 0 && n == 1) return 1;
-        Set<Integer> set = new HashSet<>();   // cannot be judge
-            
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] trustees = new int[n+1];       // how many people trust this person
+        int[] trusts = new int[n+1];         // this person trusts how many persons
+        
         for (int[] t : trust) {
-            if (!map.containsKey(t[1])) map.put(t[1], new ArrayList<>());
-            map.get(t[1]).add(t[0]);
-            set.add(t[0]);
+            trusts[t[0]]++;
+            trustees[t[1]]++;
         }
         
         for (int i = 1; i <= n; i++) {
-            if (!set.contains(i) && map.containsKey(i) && map.get(i).size() == n-1) return i;
+            if (trusts[i] == 0 && trustees[i] == n-1) return i;
         }
         
         return -1;
