feabd4d77d9619ad1f4132d10b0ad560bcca8b6e
Suvradip Paul <68487263+Suvradippaul@users.noreply.github.com>
2023-02-04 11:01:14 +0530

    Time: 3 ms (99.34%), Space: 42.5 MB (56.99%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -6,36 +6,34 @@ class Solution {
         if (n2 < n1) return false;
         
         int[] s1Chars = new int[26];
-        int noOfChars = 0;
         for (int i = 0; i < n1; i++) {
-            if (s1Chars[s1.charAt(i)-'a'] == 0) noOfChars++;
             s1Chars[s1.charAt(i)-'a']++;
         }
         
         int i = 0;
         int j = 0;
         
-        int[] s2Chars = new int[26];
         while (j < n2) {
             if (j - i < n1) {
-                s2Chars[s2.charAt(j)-'a']++;
+                s1Chars[s2.charAt(j)-'a']--;
                 j++;
             }
             else {
-                s2Chars[s2.charAt(i)-'a']--;
+                s1Chars[s2.charAt(i)-'a']++;
                 i++;
             }
             
-            if (same(s1Chars, s2Chars)) return true;
+            if (allZeroes(s1Chars)) return true;
         }
         
         return false;
     }
     
-    boolean same(int[] a, int[] b) {
+    boolean allZeroes(int[] arr) {
         for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) return false;
+            if (arr[i] != 0) return false;
         }
+        
         return true;
     }
 }
